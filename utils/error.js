25fdb27fb2bb9354e55d1d1ec3f4bcdda80bfcfe
Andrew <pasynkov2010@icloud.com>
2022-07-18 21:19:58 +0300

    change err statuses

--- utils/error.js
+++ utils/error.js
@@ -1,17 +1,17 @@
 function handleError(err, res) {
-    if (err.name === "ValidationError") {
-        res.status(400).send({ message: "Переданы некорректные данные." });
-        return;
-    }
+  if (err.name === "ValidationError") {
+    res.status(404).send({ message: "Переданы некорректные данные." });
+    return;
+  }
 
-    if (err.name === "CastError") {
-        res.status(404).send({
-            message: "Данные по запросу не удалось найти.",
-        });
-        return;
-    }
+  if (err.name === "CastError") {
+    res.status(400).send({
+      message: "Данные по запросу не удалось найти.",
+    });
+    return;
+  }
 
-    res.status(500).send({ message: "Что-то пошло не так" });
+  res.status(500).send({ message: "Что-то пошло не так" });
 }
 
 module.exports = handleError;
