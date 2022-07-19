d0f2caf2d540580d89891ae3481c9afd9a1a4611
Andrew <pasynkov2010@icloud.com>
2022-07-19 17:42:35 +0300

    fix models directory according to eslint rules

--- models/card.js
+++ models/card.js
@@ -1,34 +1,34 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const cardSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 30,
-    },
-    link: {
-        type: String,
-        required: true,
-    },
-    owner: {
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: {
+    type: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-        required: true,
-    },
-    likes: {
-        type: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "user",
-            },
-        ],
-        default: [],
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
+        ref: 'user',
+      },
+    ],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
-module.exports = mongoose.model("card", cardSchema);
+module.exports = mongoose.model('card', cardSchema);
