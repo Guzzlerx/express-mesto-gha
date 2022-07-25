6916f73995b268faf96162d987e617f9cb346958
Andrew <pasynkov2010@icloud.com>
2022-07-25 17:51:21 +0300

    fix joi avatar validation

--- routes/users.js
+++ routes/users.js
@@ -37,8 +37,11 @@ router.post(
             tlds: { allow: ['com', 'net', 'ru'] },
           }),
         password: Joi.string().required().min(5),
+        name: Joi.string().min(2).max(30),
         about: Joi.string().min(2).max(30),
-        avatar: Joi.string(),
+        avatar: Joi.string().regex(
+          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+        ),
       })
       .unknown(true),
   }),
